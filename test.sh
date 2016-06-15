134a2a6a88a1f00ea5060033688f711ddf4a5404
Tarcísio Gruppi <txgruppi@gmail.com>
2016-06-15 15:04:18 +0100

    Updated test.sh to ignore ./vendor/ folder

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "mode: atomic" > coverage.txt
 counter=0
-for package in $(find . -iname '*.go' | xargs -n 1 dirname | sort -n | uniq -c | awk '{print $2}'); do
+for package in $(find . -iname '*.go' | grep -v '^./vendor' | xargs -n 1 dirname | sort -n | uniq -c | awk '{print $2}'); do
   out="${counter}.txt"
   go test -v -coverprofile="$out" -covermode=atomic "$package"
   tail -n +2 "$out" >> coverage.txt
